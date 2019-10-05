7274ac2818f380af93a100e81f51bb49b93c146d
Phyo Thiha <phyo.thiha@mm-digital-solutions.com>
2019-10-05 08:25:25 +0630

    intial commit

---
+++ PetAdpotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetAdpotion
+//
+//  Created by Phyo Thiha on 05/10/2019.
+//  Copyright © 2019 Phyo Thiha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
